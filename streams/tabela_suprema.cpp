6ff98747c93632f16d033bbd2b23e22f65ff1379
abraao <abraao.gamer7@gmail.com>
2019-03-06 19:20:16 -0300

    Finish

--- streams/tabela_suprema.cpp
+++ streams/tabela_suprema.cpp
@@ -6,7 +6,7 @@
 
 void read_labels( std::vector<std::string>  & rotulos, std::string arq);
 void read_values(std::vector<std::string> & values, std::string arq);
-void print_head(std::vector<std::string>  & rotulos, std::vector<std::string> & value);
+void print_tab(std::vector<std::string>  & rotulos, std::vector<std::string> & value);
 
 int main(int argc, char* argv[])
 {
@@ -15,7 +15,7 @@ int main(int argc, char* argv[])
 
     read_labels(rotulos, argv[1]);
     read_values(value, argv[2]);
-    print_head(rotulos, value);
+    print_tab(rotulos, value);
 }
 
 void read_labels( std::vector<std::string>  & rotulos, std::string arq)
@@ -31,7 +31,7 @@ void read_labels( std::vector<std::string>  & rotulos, std::string arq)
 
     if( rots.fail() and !rots.eof())
     {
-        std::cout << "Erro la leitura do arquivo: " << arq << std::endl;
+        std::cout << "Erro na leitura do arquivo: " << arq << std::endl;
     }
 }
 
@@ -47,11 +47,11 @@ void read_values(std::vector<std::string> & values, std::string arq)
 
     if( vls.fail() and !vls.eof())
     {
-        std::cout << "Erro la leitura do arquivo: " << arq << std::endl;
+        std::cout << "Erro na leitura do arquivo: " << arq << std::endl;
     }
 }
 
-void print_head(std::vector<std::string>  & rotulos, std::vector<std::string> & value)
+void print_tab(std::vector<std::string>  & rotulos, std::vector<std::string> & value)
 {
     std::ostringstream line, head;
 
@@ -71,11 +71,12 @@ void print_head(std::vector<std::string>  & rotulos, std::vector<std::string> & 
     {
         for(int j{0}; j < rotulos.size(); j++)
         {
-            std::cout << "|" << std::setw((rotulos[j].size() + 1) - value[i].size()) << std::setfill(' ');
+            std::cout << "|" << std::setw((rotulos[j].size() + 1)) << std::setfill(' ');
             std::cout << value[i] << " ";
             i++;
         }
 
         std::cout << "|" << std::endl;   
     }
+    std::cout << line.str() << std::endl;
 }
