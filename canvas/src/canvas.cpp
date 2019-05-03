435ae19fbf7a54f85e35c675243407efa09cfe14
AbraaoLincoln <44592065+AbraaoLincoln@users.noreply.github.com>
2019-05-03 18:23:58 -0300

    Add files via upload

---
+++ canvas/src/canvas.cpp
@@ -0,0 +1,21 @@
+#include "../include/canvas.h"
+#include "../include/common.h"
+
+using namespace life;
+
+void Canvas::pixel(const Point2& p, const Color& c)
+{
+    auto index{p.y*(m_width*image_depth*m_block_size)+(p.x*(image_depth*m_block_size))};
+
+    for(auto i{0}; i < m_block_size;i++)
+    {
+        for(auto j{0};j < m_block_size;j++)
+        {
+            m_pixels[index] = c.channels[0];
+            m_pixels[index+1] = c.channels[1];
+            m_pixels[index+2] = c.channels[2];
+            index += 4;
+        }
+        index += ((m_width*image_depth)-(m_block_size*image_depth));
+    }
+}
